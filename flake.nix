b7e13f9692fbdd3513f2a480c5d12fbb9d9902af
Károly Sárdi <skver@tuta.io>
2024-03-29 10:58:57 +0100

    add dev env flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "php dev env flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+          {
+            devShells.default = pkgs.mkShell {
+              buildInputs = with pkgs;
+                [
+                  git
+                  php
+                  php.packages.composer
+                ];
+            };
+          }
+    );
+}
